2079645e3dcc552d076bcf7534307b6dc04a042c
MrJustreborn <rene.lottes@gmx.de>
2017-11-18 22:23:29 +0100

    Added dir view for PCKInspectorDock

--- addons/PCKManager/assets/PCKInspectorDock.gd
+++ addons/PCKManager/assets/PCKInspectorDock.gd
@@ -2,19 +2,10 @@ tool
 extends Control
 
 onready var tree = get_node("VBoxContainer/VSplitContainer/Tree")
+onready var itemList = get_node("VBoxContainer/VSplitContainer/ItemList")
 onready var lineEdit = get_node("VBoxContainer/HBoxContainer/LineEdit")
 var PCKDir = preload("res://addons/PCKManager/PCKDirectory.gd")
 var pckDir = PCKDir.new(false)
-
-func _ready():
-	var item = tree.create_item()
-	item.set_text(0,"1")
-	
-	var item2 = tree.create_item(item)
-	item2.set_text(0,"2")
-	
-	var item3 = tree.create_item(item)
-	item3.set_text(0,"1-2")
 
 func _parseFiles():
 	tree.clear()
@@ -24,8 +15,10 @@ func _parseFiles():
 	n = n[n.size()-1]
 	root.set_text(0,n)
 	
+	var drive_names = []
 	for drives in pckDir.get_drive_count():
 		var drive = pckDir.get_drive(drives)
+		drive_names.append(drive)
 		var item = tree.create_item(root)
 		item.set_text(0,drive)
 		
@@ -33,31 +26,35 @@ func _parseFiles():
 		pckDir.list_dir_begin(true)
 		var s = pckDir.get_next()
 		var files = []
+		var dirs = []
 		while(s != ""):
 			if pckDir.current_is_dir():
 				var dir = tree.create_item(item)
 				dir.set_text(0,s)
-				files.append(s)
+				dirs.append(s)
 				_addDir(dir, pckDir.get_current_dir()+s, PCKDir.new(false,pckDir.get_raw()))
 			else:
 				files.append(s)
 			s = pckDir.get_next()
-		var file = tree.create_item(item)
-		file.set_text(0,str(files))
+		item.set_metadata(0,{"dirs":dirs,"files":files})
+	root.set_metadata(0,{"dirs":drive_names,"files":{}})
 
 func _addDir( root, path, pckDirAccess ):
 	pckDirAccess.change_dir(path)
 	pckDirAccess.list_dir_begin(true)
 	var s = pckDirAccess.get_next()
+	var files = []
+	var dirs = []
 	while(s != ""):
 		if pckDirAccess.current_is_dir():
 			var dir = tree.create_item(root)
 			dir.set_text(0,s)
+			dirs.append(s)
 			_addDir(dir, pckDirAccess.get_current_dir()+s, PCKDir.new(false,pckDir.get_raw()))
 		else:
-			var file = tree.create_item(root)
-			file.set_text(0,s)
+			files.append(s)
 		s = pckDirAccess.get_next()
+	root.set_metadata(0,{"dirs":dirs,"files":files})
 
 func _on_Button_pressed():
 	$FileDialog.popup()
@@ -73,5 +70,19 @@ func _on_FileDialog_file_selected( path ):
 
 
 func _on_Tree_item_selected():
-	print(tree.get_selected().get_text(0))
-	print(tree.get_selected().get_parent().get_text(0))
+	itemList.clear()
+	var data = tree.get_selected().get_metadata(0)
+	if data == null:
+		return
+	var dirs = data["dirs"]
+	var files = data["files"]
+	
+	for d in dirs:
+		itemList.add_item(d)
+	for f in files:
+		itemList.add_item(f)
+
+
+func _on_ItemList_item_activated( index ):
+	itemList.get_item_text(index)
+	tree.get_selected().get_parent().select(0)
