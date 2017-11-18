1c0acb8e7a2074f486eadaaef9a95ccc6fcf3bfd
MrJustreborn <rene.lottes@gmx.de>
2017-11-18 18:06:14 +0100

    Impl PCKDirectory

---
+++ PCKDir_test.gd
@@ -0,0 +1,40 @@
+extends Node
+
+
+func _ready():
+	var dir = load("res://PCKDirectory.gd").new()
+	
+	dir.change_dir("res://hud/test/")
+	dir.list_dir_begin(true)
+	
+	print("===list dirs===")
+	
+	var s = dir.get_next()
+	while(s != ""):
+		print(s, " -> ", dir.current_is_dir())
+		s = dir.get_next()
+	
+	dir.change_dir(".././/")
+	dir.list_dir_begin(true)
+	
+	s = dir.get_next()
+	while(s != ""):
+		print(s, " -> ", dir.current_is_dir())
+		s = dir.get_next()
+	
+	print("===add dirs===")
+	
+	print("common: ",dir.get_common_paths("res://test.pck"))
+	dir.add_pck("res://test.pck",false)
+	
+	print("===list new dirs===")
+	
+	dir.change_dir("res://")
+	dir.list_dir_begin(true)
+	
+	s = dir.get_next()
+	while(s != ""):
+		print(s, " -> ", dir.current_is_dir())
+		s = dir.get_next()
+	
+
---
+++ pck.gd
@@ -0,0 +1,55 @@
+tool
+extends EditorScript
+
+
+func _run():
+	var packer = PCKPacker.new()
+	packer.pck_start("res://test.pck",0)
+	packer.add_file("res://icon.png","res://icon.png")
+	packer.add_file("res://icon2.png","res://icon.png")
+	packer.flush(true)
+	
+	
+	#{GDPC}{int32_version}{int32_major}{int32_minor}{int32_revision} 16x{int32_reserved}{files_count}
+	#{Path-String}
+	
+	print("\n\n===============\n")
+	
+	var file = File.new()
+	
+	#file.set_endian_swap(true)
+	file.open("res://test.pck",File.READ)
+	
+	print(str(file.get_32() == 0x43504447))
+	print("Version: ",file.get_32()," / Major: ",file.get_32()," / Minor: ",file.get_32()," / Revision: ",file.get_32())
+	
+	for i in range(16):
+		file.get_32()
+	
+	var file_count = file.get_32()
+	print("Files: ", file_count)
+	
+	for i in range(file_count):
+		#print("__-- File: ",i," --__")
+		var length = file.get_32()
+		#print("length: ", length)
+		
+		var path = file.get_buffer(length).get_string_from_utf8()
+		print("Path: ", path)
+		
+		var offset =  file.get_64()
+		var size = file.get_64()
+		var md5 = file.get_buffer(16)
+		#print("offset: ", offset, " / Size: ", size, " / md5: ",md5)
+	
+	#file.seek(189)
+	#var png = file.get_buffer(3498)
+	
+	#print(png.size())
+	
+	file.close()
+	
+	print("\n\n===============\n")
+	
+	pass
+
