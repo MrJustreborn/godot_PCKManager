cd85b646a494c1ab0f92fc0f39288442dd4bca73
Eugene Frost <38176435+TuzelKO@users.noreply.github.com>
2025-07-22 16:34:02 +0300

    Update PCKManager.gd
    Add PCKLoader/autoload property in ProjectSettings

--- addons/PCKManager/PCKManager.gd
+++ addons/PCKManager/PCKManager.gd
@@ -8,7 +8,9 @@ var PckCustomizer = preload("res://addons/PCKManager/PCKCustomizer/pck_customize
 
 func _enter_tree() -> void:
 	managerDock = preload("res://addons/PCKManager/assets/PCKManagerDock.tscn").instantiate()
-	
+
+	_add_project_settings()
+
 	add_autoload_singleton("PCKLoader", "res://addons/PCKManager/pck_loader.gd")
 	add_export_plugin(PckCustomizer)
 	EditorInterface.get_editor_main_screen().add_child(managerDock)
@@ -19,6 +21,17 @@ func _exit_tree() -> void:
 	
 	remove_autoload_singleton("PCKLoader")
 	remove_export_plugin(PckCustomizer)
+
+func _add_project_settings() -> void:
+	if (!ProjectSettings.has_setting("PCKLoader/autoload")):
+		ProjectSettings.set_setting("PCKLoader/autoload", true)
+	ProjectSettings.add_property_info({
+		"name": "PCKLoader/autoload",
+		"type": TYPE_BOOL
+	})
+	ProjectSettings.set_initial_value("PCKLoader/autoload", true)
+	ProjectSettings.set_as_basic("PCKLoader/autoload", true)
+	ProjectSettings.save()
 
 func _has_main_screen() -> bool:
 	return true
