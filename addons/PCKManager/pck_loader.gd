3d9cb14a3a8332b054e093abe8b623c4592a7b5a
Eugene Frost <38176435+TuzelKO@users.noreply.github.com>
2025-07-22 16:36:31 +0300

    Update pck_loader.gd
    Add auto-loading disabling and manual load func with configurable path

--- addons/PCKManager/pck_loader.gd
+++ addons/PCKManager/pck_loader.gd
@@ -1,12 +1,24 @@
 extends Node
 
-
-func _init() -> void:
-	var base_path := OS.get_executable_path().get_base_dir()
+func _init(load: bool = false) -> void:
+	if !load:
+		if ProjectSettings.has_setting("PCKLoader/autoload"):
+			load = ProjectSettings.get("PCKLoader/autoload")
+		
+		if !load:
+			prints("Autoload disabled! - Skipping PCK initial loading")
+			return
+	
 	if OS.has_feature("editor") and false:
 		prints("Running from editor! - Skipping PCK loading")
 	else:
-		_load_pck_files_recursive(base_path)
+		load_pck_files()
+
+func load_pck_files(path: String = "") -> void:
+	var base_path := OS.get_executable_path().get_base_dir()
+	if !path.is_empty():
+		base_path += "/" + path + "/"
+	_load_pck_files_recursive(base_path)
 
 func _load_pck_files_recursive(path: String) -> void:
 	var base_pck := OS.get_executable_path().get_basename() + ".pck"
