f6da25a06c4b0d10660137167ec4c556e87bf2e3
Eugene Frost <38176435+TuzelKO@users.noreply.github.com>
2025-07-26 09:31:19 +0300

    Add autoload disabling and manual loading function (#3)
    * Update PCKManager.gd
    
    Add PCKLoader/autoload property in ProjectSettings
    
    * Update pck_loader.gd
    
    Add auto-loading disabling and manual load func with configurable path
    
    * Update pck_loader.gd
    
    Woops...
    
    * Update pck_loader.gd
    
    Woops 2.0...
    
    * Update pck_loader.gd
    
    Add loading errors return and errors suppression for backward compatibility
    
    * Update pck_loader.gd
    
    Corrections according to review
    
    * Update PCKManager.gd
    
    Corrections according to review

--- addons/PCKManager/PCKManager.gd
+++ addons/PCKManager/PCKManager.gd
@@ -8,7 +8,9 @@ var PckCustomizer = preload("res://addons/PCKManager/PCKCustomizer/pck_customize
 
 func _enter_tree() -> void:
 	managerDock = preload("res://addons/PCKManager/assets/PCKManagerDock.tscn").instantiate()
-	
+
+	_add_project_settings()
+
 	add_autoload_singleton("PCKLoader", "res://addons/PCKManager/pck_loader.gd")
 	add_export_plugin(PckCustomizer)
 	EditorInterface.get_editor_main_screen().add_child(managerDock)
@@ -19,6 +21,17 @@ func _exit_tree() -> void:
 	
 	remove_autoload_singleton("PCKLoader")
 	remove_export_plugin(PckCustomizer)
+
+func _add_project_settings() -> void:
+	if (!ProjectSettings.has_setting("pck_manager/pck_loader/autoload_all_pck")):
+		ProjectSettings.set_setting("pck_manager/pck_loader/autoload_all_pck", true)
+	ProjectSettings.add_property_info({
+		"name": "pck_manager/pck_loader/autoload_all_pck",
+		"type": TYPE_BOOL
+	})
+	ProjectSettings.set_initial_value("pck_manager/pck_loader/autoload_all_pck", true)
+	ProjectSettings.set_as_basic("pck_manager/pck_loader/autoload_all_pck", true)
+	ProjectSettings.save()
 
 func _has_main_screen() -> bool:
 	return true
--- addons/PCKManager/pck_loader.gd
+++ addons/PCKManager/pck_loader.gd
@@ -2,24 +2,38 @@ extends Node
 
 
 func _init() -> void:
-	var base_path := OS.get_executable_path().get_base_dir()
+	var load = ProjectSettings.get_setting("pck_manager/pck_loader/autoload_all_pck", true)
+	
+	if !load:
+		prints("Autoload disabled! - Skipping PCK initial loading")
+		return
+	
 	if OS.has_feature("editor") and false:
-		prints("Running from editor! - Skipping PCK loading")
+		prints("Running from editor! - Skipping PCK initial loading")
 	else:
-		_load_pck_files_recursive(base_path)
+		load_pck_files()
 
-func _load_pck_files_recursive(path: String) -> void:
+func load_pck_files(path: String = "", suppress_errors: bool = true) -> bool:
+	var base_path := OS.get_executable_path().get_base_dir()
+	if !path.is_empty():
+		base_path += "/" + path
+	return _load_pck_files_recursive(base_path, suppress_errors)
+
+func _load_pck_files_recursive(path: String, suppress_errors: bool) -> bool:
 	var base_pck := OS.get_executable_path().get_basename() + ".pck"
 	var dir := DirAccess.open(path)
 	if dir == null:
 		push_error("Failed to open directory: " + path)
-		return
+		if !suppress_errors: 
+			return false
 
 	dir.list_dir_begin()
 	var file_name := dir.get_next()
 	while file_name != "":
 		if dir.current_is_dir() and file_name != "." and file_name != "..":
-			_load_pck_files_recursive(path + "/" + file_name)
+			var result := _load_pck_files_recursive(path + "/" + file_name, suppress_errors)
+			if !suppress_errors and !result:
+				return false
 		elif file_name.to_lower().ends_with(".pck"):
 			var full_path := path + "/" + file_name
 			if full_path == base_pck:
@@ -29,5 +43,9 @@ func _load_pck_files_recursive(path: String) -> void:
 			var success := ProjectSettings.load_resource_pack(full_path)
 			if not success:
 				push_error("Failed to load: " + full_path)
+				if !suppress_errors:
+					return false
 		file_name = dir.get_next()
 	dir.list_dir_end()
+	
+	return true
